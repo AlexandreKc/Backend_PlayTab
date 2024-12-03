bdf5334f85723cd7e1c1632e9d4291b1329b9db9
Sebastian <sebastiancarrera2021@gmail.com>
2024-12-03 20:28:53 -0300

    x2

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const port = 3000;
 const bcrypt = require('bcryptjs');
 require('dotenv').config();
-const allowedOrigins = ['http://localhost', 'http://backendplaytab-production.up.railway.app'];
+const allowedOrigins = ['https://localhost', 'http://backendplaytab-production.up.railway.app'];
 app.use(cors({
   origin: (origin, callback) => {
     if (!origin || allowedOrigins.includes(origin)) {
